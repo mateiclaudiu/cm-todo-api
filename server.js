4e41ef9a66a9956741e73d503d67f1239e9bfb22
Claudiu Matei <claudiu.matei@optis.be>
2016-10-11 14:56:50 +0200

    underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore'); 
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,14 +21,17 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id, 10); // params.id is a string and id is a number
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id:todoId});
+
+	/*var matchedTodo;
 
 	todos.forEach(function(todo){
 		if(todo.id === todoId){
 			matchedTodo=todo;
 
 		}
-	});
+	});*/
+
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -38,8 +43,13 @@ app.get('/todos/:id', function(req,res){
 
 // POST /todos
 app.post('/todos', function (req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description','completed');//keep only the given values
 
+	if(!_.isBoolean(body.completed || !_.isString(body.description)) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description=body.description.trim();
 	body.id=todoNextId++;
 
 	todos.push(body);
