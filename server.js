e9bd71d94deebd6c01dffe3c1e2642b813e20112
Claudiu Matei <claudiu.matei@optis.be>
2016-10-11 16:18:17 +0200

    update

--- server.js
+++ server.js
@@ -71,6 +71,36 @@ app.delete('/todos/:id', function(req, res){
 	res.json(matchedTodo);
 });
 
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10); // params.id is a string and id is a number
+	var matchedTodo = _.findWhere(todos, {id:todoId})
+	var body = _.pick(req.body, 'description','completed');//keep only the given values
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).json({'error':'no todo foud with that id'});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	} 
+
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	} 
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
