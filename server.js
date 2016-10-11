be9fbd03e9f4f6e128273fb026862d35eba9fdfb
Claudiu Matei <claudiu.matei@optis.be>
2016-10-11 13:44:43 +0200

    add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+var todos = [];
+var todoNextId=1;
+
+app.use(bodyParser.json()); // application-middleware, every time a json request comes in, it will be parsed to json
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -43,6 +35,18 @@ app.get('/todos/:id', function(req,res){
 	}
 
 });
+
+// POST /todos
+app.post('/todos', function (req, res){
+	var body = req.body;
+
+	body.id=todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
