54037996117e895cd531cb04b4213106b1a76286
Claudiu Matei <claudiu.matei@optis.be>
2016-10-14 16:24:52 +0200

    added completed filter

--- server.js
+++ server.js
@@ -13,9 +13,19 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed==='true'){
+		filteredTodos = _.where(filteredTodos, {completed:true});
+	}
+	else if(queryParams.hasOwnProperty('completed') && queryParams.completed==='false'){
+		filteredTodos = _.where(filteredTodos, {completed:false});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -88,7 +98,7 @@ app.put('/todos/:id', function(req, res){
 	} else if (body.hasOwnProperty('completed')){
 		return res.status(400).send();
 	} 
-
+ 
 
 	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >0){
 		validAttributes.description = body.description;
