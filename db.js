4d9732d9272776ad45c364661a012affb0fd0854
Claudiu Matei <claudiu.matei@optis.be>
2016-10-18 19:56:17 +0200

    add postgress for prodction

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	dialect: 'sqlite',
-	storage: __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		dialect: 'sqlite',
+		storage: __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
+
+
 
 var db = {};
 
-db.todo = sequelize.import(__dirname+'/models/todo.js'); //load sequalize models from different files
+db.todo = sequelize.import(__dirname + '/models/todo.js'); //load sequalize models from different files
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
