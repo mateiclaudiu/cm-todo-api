864955e109d3cc63688ef4609d36e77ac6aaa724
Claudiu Matei <claudiu.matei@optis.be>
2016-10-20 10:26:07 +0200

    login promise

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -139,9 +141,20 @@ app.post('/users', function(req, res) {
 		});
 })
 
+// POST /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user) {
+		res.json(user.toPublicJSON());
+	}, function() {
+		res.status(401).send();
+	});
+});
+
 
 db.sequelize.sync({
-	logging: console.log, 
+	logging: console.log,force:true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
